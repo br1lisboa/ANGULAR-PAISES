68b17a1a88706f265c17c419173a24b1bb050fe5
Lisboa <b.lisboa@accenture.com>
2022-12-21 06:57:14 -0300

    feat: service api

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-pais',
@@ -7,10 +8,15 @@ import { Component } from '@angular/core';
 })
 export class PorPaisComponent {
 
-  termino: string = 'Hola Mundo'
+  termino: string = ''
+
+  constructor(private paisService: PaisService) { }
 
   buscar() {
-    console.log(this.termino)
+
+    this.paisService.buscarPais(this.termino)
+      .subscribe(resp => console.log(resp))
+
   }
 
 }
